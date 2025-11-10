c0e413223eb53846459a9c7f00adc97510187519
LuckedCoronet <lc45@lc-studios.net>
2025-11-10 17:25:14 +0900

    fix(blocks): bomb activator

--- src/bp/scripts/core/block_components/index.ts
+++ src/bp/scripts/core/block_components/index.ts
@@ -1,6 +1,7 @@
 import "./air_conditioner";
 import "./alarm";
 import "./big_number_sign";
+import "./bomb_activator";
 import "./curve_block";
 import "./legacy_placement_state";
 import "./old_door_conversion";
