14ec7b37a1df7ebcf64ffa03c08f18ae64501be6
LuckedCoronet <lc45@lc-studios.net>
2025-12-01 17:47:08 +0900

    fix(items): scar

--- src/bp/scripts/core/item_components/index.ts
+++ src/bp/scripts/core/item_components/index.ts
@@ -21,6 +21,7 @@ import "./gun/mp5k";
 import "./gun/mp7a1";
 import "./gun/p90";
 import "./gun/rocket_launcher";
+import "./gun/scar";
 import "./dont_damage_durability_on_hit";
 import "./lose_durability_on_dig";
 import "./medkit";
