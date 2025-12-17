3d11f683ab118570e0b76ba26b5dc00231472aad
LuckedCoronet <lc45@lc-studios.net>
2025-12-17 09:09:09 +0900

    fix(blocks): pipe placement

--- src/bp/scripts/core/block_components/pipe.ts
+++ src/bp/scripts/core/block_components/pipe.ts
@@ -3,13 +3,16 @@ import * as mc from "@minecraft/server";
 
 mc.system.beforeEvents.startup.subscribe((e) => {
 	e.blockComponentRegistry.registerCustomComponent("scpdt:pipe", {
-		onPlace({ block }) {
-			const blockFace = block.permutation.getState("minecraft:block_face");
+		beforeOnPlayerPlace(arg) {
+			const blockFace = arg.permutationToPlace.getState("minecraft:block_face");
 
 			// @ts-expect-error
 			const legacyFacingDir: number = LEGACY_FACING_DIRECTION_INDEX[blockFace];
 
-			block.setPermutation(block.permutation.withState("facing:direction", legacyFacingDir));
+			arg.permutationToPlace = arg.permutationToPlace.withState(
+				"facing:direction",
+				legacyFacingDir,
+			);
 		},
 	});
 });
